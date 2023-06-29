d55ea1fed4a2673f7ad3a3bd66e323322cecc9f3
“Daniel <danieljuarez1996@gmail.com>
2023-06-29 08:45:18 -0300

    correcciones1

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using ClaseJson;
+using System.Net;
+using System.Text.Json;
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
@@ -10,14 +12,31 @@ personajes1.role = "";
 personajes1.lore = "";
 personajes1.abilities = new List<string>();
 
-//leer el archivo txt que devuelve el string
+ try
+            {
 
-//le envio al deserealizador
+                    using (Stream strReader = response.GetResponseStream())
+                    {
+                        if (strReader == null) return;
+                        using (StreamReader objReader = new StreamReader(strReader))
+                        {
+                            string Lineas = objReader.ReadToEnd();
+                            Personajes personajes = JsonSerializer.Deserialize<Personajes>(Lineas);
 
-//
+                                
+                                Console.WriteLine("Nombre: " + personajes.name );
+                                Console.WriteLine("Rol: " + personajes.role );
+                                Console.WriteLine("Lore: " + personajes.lore);
+                                Console.WriteLine("Lore: " + personajes.lore);
+                                Console.WriteLine("HAbilidad1: " + personajes.abilities);
+                                
 
-//
-
-//
-
-
+                        }
+                    }
+                }
+            }
+            catch (WebException ex)
+            {
+                Console.WriteLine("Problemas de acceso a la API");
+            }
+        
